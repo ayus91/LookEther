8838842961ec5aef10841266a52531d76354d24e
ayus91 <jihaleeb1@gmail.com>
2020-08-28 17:44:02 +0900

    Fix Temp Algorithm, API response
     - UVI에 따른 추천 방식 추가 및 온도에 따른 추천 세분화
     - API응답시 받아오는 정보 수정

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import { Toggle } from "./pages/Toggle";
 import Signin from "./pages/Signin";
 import { Signup } from "./pages/Signup";
 import Contentpage from "./pages/Contentpage";
-import {Signout} from "./pages/Signout"
+import { Signout } from "./pages/Signout";
 import axios from "axios";
 
 // import { WEATHER_API_KEY } from "../config/config";
@@ -19,177 +19,191 @@ import "./App.css";
 dotenv.config({ path: path.join(__dirname, "path/to/.env") });
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isLogin: false,
-			userInfo: {
-				email: "",
-				password: "",
-				username: "",
-				gender: "",
-			},
-			currentWeatherInfo: {
-				date: "",
-				icon: "",
-				temp: "",
-				feelslike: "",
-				min: "",
-				max: "",
-				rain: "",
-			},
-			dailyWeatherInfo: [],
-			location: {
-				city: "",
-			},
-		};
-		this.getCurrentWeather = this.getCurrentWeather.bind(this);
-		this.handdleUserInfo = this.handdleUserInfo.bind(this);
-		this.handleLogin = this.handleLogin.bind(this);
-		this.handleLogout = this.handleLogout.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogin: false,
+      userInfo: {
+        email: "",
+        password: "",
+        username: "",
+        gender: "",
+      },
+      currentWeatherInfo: {
+        date: "",
+        icon: "",
+        temp: "",
+        feelslike: "",
+        min: "",
+        max: "",
+        rain: "",
+      },
+      dailyWeatherInfo: [],
+      location: {
+        city: "",
+      },
+    };
+    this.getCurrentWeather = this.getCurrentWeather.bind(this);
+    this.handdleUserInfo = this.handdleUserInfo.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
 
+  handleLogin = () => {
+    this.setState({ isLogin: true });
+  };
 
+  handleLogout = () => {
+    this.setState({ isLogin: false });
+    localStorage.clear();
+    return <Redirect path="*" to="/" />;
+  };
 
+  handdleUserInfo(user) {
+    this.setState({
+      userInfo: user,
+    });
+  }
 
-	handleLogin = () => {
-		this.setState({ isLogin: true });
-	};
+  hiddenMain() {
+    this.setState({
+      showmain: false,
+    });
+  }
+  //var d = new Date(unixtime*1000); dt 변경
+  getCurrentWeather = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        getWeatherData(
+          {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+            key: process.env.REACT_APP_WEATHER_API_KEY,
+          },
+          (data) => {
+            this.setState({
+              currentWeatherInfo: {
+                date: new Date(data.current.dt * 1000),
+                icon: data.current.weather[0].icon,
+                temp: data.current.temp,
+                uvi: data.current.uvi,
+                rain: data.current.rain,
+                feelslike: data.current.feels_like,
+                min: data.daily[0].temp.min,
+                max: data.daily[0].temp.max,
+              },
+              dailyWeatherInfo: data.daily.slice(1, 6),
+            });
+          }
+        );
+        reverseGeo(
+          {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+          },
+          (data) => {
+            this.setState({
+              location: { city: data.principalSubdivision },
+            });
+          }
+        );
+      });
+    } else {
+      alert("현재 위치 정보가 현재 브라우저에서 지원하지 않습니다.");
+    }
+  };
 
-	handleLogout = () => {
-		this.setState({ isLogin: false });
-		localStorage.clear()
-		return(
-			<Redirect path="*" to="/"/>
-		)
-	};
+  componentDidMount() {
+    this.getCurrentWeather();
+    let id = localStorage.getItem("userInfo");
+    if (id) {
+      this.handleLogin();
+    } else {
+      this.handleLogout();
+    }
+  }
 
-	handdleUserInfo(user) {
-		this.setState({
-			userInfo: user,
-		});
-	}
+  render() {
+    // const { isLogin , userInfo } = this.state;
+    const { isLogin } = this.state;
+    return (
+      // title, location, signin, id=nav
+      // contentpage
+      // weekly
+      // if it clicks signin, nav exists, signin box appears instead of contentpage
+      // if it successes signin, contentpage appears back
+      // if it doesn't have id, signup page appears
+      // if it's done signup, signin page appears back
 
-	hiddenMain() {
-		this.setState({
-			showmain: false,
-		});
-	}
-	//var d = new Date(unixtime*1000); dt 변경
-	getCurrentWeather = () => {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition((position) => {
-				getWeatherData(
-					{
-						lat: position.coords.latitude,
-						lon: position.coords.longitude,
-						key: process.env.REACT_APP_WEATHER_API_KEY,
-					},
-					(data) => {
-						this.setState({
-							currentWeatherInfo: {
-								date: new Date(data.current.dt * 1000),
-								icon: data.current.weather[0].icon,
-								temp: data.current.temp,
-								feelslike: data.current.feels_like,
-								min: data.daily[0].temp.min,
-								max: data.daily[0].temp.max,
-							},
-							dailyWeatherInfo: data.daily.slice(1,6),
-						});
-					}
-				);
-				reverseGeo(
-					{
-						lat: position.coords.latitude,
-						lon: position.coords.longitude,
-					},
-					(data) => {
-						this.setState({
-							location: { city: data.principalSubdivision },
-						});
-					}
-				);
-			});
-		} else {
-			alert("현재 위치 정보가 현재 브라우저에서 지원하지 않습니다.");
-		}
-	};
-	
-
-	componentDidMount() {
-		this.getCurrentWeather();
-		let id = localStorage.getItem('userInfo')
-		if(id){
-			this.handleLogin()
-		}else{
-			this.handleLogout()
-		}
-	}
-
-	render() {
-		// const { isLogin , userInfo } = this.state;
-		const { isLogin } = this.state;
-		return (
-			// title, location, signin, id=nav
-			// contentpage
-			// weekly
-			// if it clicks signin, nav exists, signin box appears instead of contentpage
-			// if it successes signin, contentpage appears back
-			// if it doesn't have id, signup page appears
-			// if it's done signup, signin page appears back
-			
-			<div>
-				<div id="nav" className="d-flex justify-content-between">
-						<Link to="/" className="h3">LookEther</Link>
-						<Toggle city={this.state.location.city} />
-						<div>
-
-								{isLogin ? (
-								<div>
-								<div type="button" className="P2 btn btn-light btn-lg">Mypage</div>,
-								<div type="button" className="P2 btn btn-light btn-lg" onClick ={this.handleLogout}>Signout</div>
-								</div>)
-								: <Link to ="/signin" type="button" className="P2 btn btn-light btn-lg">Signin</Link>}
-						</div>
-				</div>
-				<Switch>
-					<Route
-						exact
-						path="/"
-						render={() => {
-							return (
-								<div>
-									<div>
-										<Contentpage
-											weather={this.state.currentWeatherInfo}
-											daily={this.state.dailyWeatherInfo}
-											userinfo={this.state.userInfo}
-										/>
-									</div>
-								</div>
-							);
-						}}
-					/>
-					<Route
-						path="/signin"
-						render={() => {
-							return (
-								<Signin
-									isLogin={isLogin}
-									userInfo={this.state.userInfo}
-									handdleUserInfo={this.handdleUserInfo}
-									handleLogin={this.handleLogin}
-								/>
-							);
-						}}
-					/>
-					<Route path="/signup" component={Signup} />
-					<Redirect path="*" to="/"/>
-				</Switch>
-			</div>
-		);
-	}
+      <div>
+        <div id="nav" className="d-flex justify-content-between">
+          <Link to="/" className="h3">
+            LookEther
+          </Link>
+          <Toggle city={this.state.location.city} />
+          <div>
+            {isLogin ? (
+              <div>
+                <div type="button" className="P2 btn btn-light btn-lg">
+                  Mypage
+                </div>
+                ,
+                <div
+                  type="button"
+                  className="P2 btn btn-light btn-lg"
+                  onClick={this.handleLogout}
+                >
+                  Signout
+                </div>
+              </div>
+            ) : (
+              <Link
+                to="/signin"
+                type="button"
+                className="P2 btn btn-light btn-lg"
+              >
+                Signin
+              </Link>
+            )}
+          </div>
+        </div>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return (
+                <div>
+                  <div>
+                    <Contentpage
+                      weather={this.state.currentWeatherInfo}
+                      daily={this.state.dailyWeatherInfo}
+                      userinfo={this.state.userInfo}
+                    />
+                  </div>
+                </div>
+              );
+            }}
+          />
+          <Route
+            path="/signin"
+            render={() => {
+              return (
+                <Signin
+                  isLogin={isLogin}
+                  userInfo={this.state.userInfo}
+                  handdleUserInfo={this.handdleUserInfo}
+                  handleLogin={this.handleLogin}
+                />
+              );
+            }}
+          />
+          <Route path="/signup" component={Signup} />
+          <Redirect path="*" to="/" />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/pages/Todayclothes.js
+++ client/src/pages/Todayclothes.js
@@ -1,38 +1,66 @@
-import React from "react"
-import DefaultImg from "./DefaultImg"
-import {clothesAll}  from "../../public/defaultClothes/clothesAll"
-import "./Todayclothes.css"
+import React from "react";
+import DefaultImg from "./DefaultImg";
+import { clothesAll } from "../../public/defaultClothes/clothesAll";
+import "./Todayclothes.css";
 
 const Todayclothes = (props) => {
-	let defaultClothes = {
-		shirts: "",
-		pants: "",
-	}
-	if (props.weather >= 22) {
-		defaultClothes.etc = clothesAll.summerhat
-		defaultClothes.shirts = clothesAll.shortShirts
-		defaultClothes.pants = clothesAll.shortPants
-	} else if (22 > props.weather && props.weather >=17) {
-		defaultClothes.shortShirts = clothesAll.shortShirts
-		defaultClothes.pants = clothesAll.pants
-	} else if (17>= props.weather && props.weather >=12) {
-		defaultClothes.etc = clothesAll.cardigan
-		defaultClothes.shirts = clothesAll.longShirts
-		defaultClothes.pants = clothesAll.pants
-	} else if (12>= props.weather && props.weather >=9) {
-		defaultClothes.etc = clothesAll.jacket
-		defaultClothes.shirts = clothesAll.hoodie
-		defaultClothes.pants = clothesAll.pants
-	} else if (9 >= props.weather && props.weather !== "") {
-		defaultClothes.etc = clothesAll.wintrtjacket
-		defaultClothes.shirts = clothesAll.hoodie
-		defaultClothes.pants = clothesAll.pants
-	}
-	return (
-				<div id="cloth-icon" >
-				<DefaultImg default={defaultClothes} />
-				</div>
-	)
-}
+  let defaultClothes = {
+    shirts: "",
+    pants: "",
+    outer: "",
+    shoes: "",
+    etc: "",
+  };
 
-export default Todayclothes
+  const weather = props.weather.weather;
+
+  if (5 < weather.uvi) {
+    defaultClothes.etc = clothesAll.summerhat;
+  }
+  if (5 >= weather.uvi) {
+    defaultClothes.etc = clothesAll.sunscreen;
+  }
+  if (weather.rain) {
+    defaultClothes.etc = clothesAll.umbralla;
+  }
+  if (weather.temp >= 28) {
+    defaultClothes.shirts = clothesAll.sleeveless;
+    defaultClothes.pants = clothesAll.shortPants;
+    defaultClothes.shoes = clothesAll.slipper;
+  } else if (28 > weather.temp && weather.temp >= 23) {
+    defaultClothes.shirts = clothesAll.shortShirts;
+    defaultClothes.pants = clothesAll.shortPants;
+    defaultClothes.shoes = clothesAll.slipper;
+  } else if (23 > weather.temp && weather.temp >= 20) {
+    defaultClothes.shirts = clothesAll.longShirts;
+    defaultClothes.pants = clothesAll.pants;
+    defaultClothes.shoes = clothesAll.shoes;
+  } else if (20 > weather.temp && weather.temp >= 17) {
+    defaultClothes.outer = clothesAll.cardigan;
+    defaultClothes.shirts = clothesAll.longShirts;
+    defaultClothes.pants = clothesAll.pants;
+    defaultClothes.shoes = clothesAll.shoes;
+  } else if (17 > weather.temp && weather.temp >= 12) {
+    defaultClothes.outer = clothesAll.coat;
+    defaultClothes.shirts = clothesAll.hoodie;
+    defaultClothes.pants = clothesAll.pants;
+    defaultClothes.shoes = clothesAll.shoes;
+  } else if (12 > weather.temp && weather.temp >= 9) {
+    defaultClothes.outer = clothesAll.coat;
+    defaultClothes.shirts = clothesAll.sweater;
+    defaultClothes.pants = clothesAll.winterpnats;
+    defaultClothes.shoes = clothesAll.shoes;
+  } else if (9 >= weather.temp && weather.temp !== "") {
+    defaultClothes.outer = clothesAll.wintrtjacket;
+    defaultClothes.shirts = clothesAll.sweater;
+    defaultClothes.pants = clothesAll.winterpnats;
+    defaultClothes.shoes = clothesAll.ugg;
+  }
+  return (
+    <div id="cloth-icon">
+      <DefaultImg default={defaultClothes} />
+    </div>
+  );
+};
+
+export default Todayclothes;
